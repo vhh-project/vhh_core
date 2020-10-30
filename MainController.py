85a6b72bd861ae0a97683a1fbe7a989ad64eadfe
User <dhelm@zapp.cvl.tuwien.ac.at>
2020-10-30 09:53:55 +0100

    Bugfix to read and interprete created csv results correctly (shot_id vs. movie_id)

--- MainController.py
+++ MainController.py
@@ -77,10 +77,11 @@ class MainController(object):
 
         # merge all results
         results_np = self.merge_results()
-        header = ["shot_id", "vid_name", "start", "end", "stc", "cmc"]
+        header = ["vid_name", "shot_id", "start", "end", "stc", "cmc"]
 
-        vids = np.unique(results_np[:, 1:2])
-
+        vids = np.unique(results_np[:, :1])
+        print(vids)
+        
         if self.__configuration_instance.results_format == "CSV_LOCAL":
 
             # write Output to .csv file
@@ -98,7 +99,7 @@ class MainController(object):
 
             # create dictionary as basis for JSON file
             for vid in vids:
-                indices = np.where(vid == results_np[:, 1])[0]
+                indices = np.where(vid == results_np[:, 0])[0]
                 vid_results_np = results_np[indices]
 
                 vid_results_dict = {}
@@ -125,12 +126,12 @@ class MainController(object):
             print(f"Posting results using Rest API...")
             # post all results
             for vid in vids:
-                indices = np.where(vid == results_np[:, 1])[0]
+                indices = np.where(vid == results_np[:, 0])[0]
                 vid_results_np = results_np[indices]
 
                 header_np = np.expand_dims(np.array(header), axis=0)
                 vid_results_np = np.concatenate((header_np, vid_results_np), axis=0)
-
+                print(vid_results_np)
                 self.__rest_api_instance.postAutomaticResults(vid=int(vid.split('.')[0]), results_np=vid_results_np)
 
         else:
@@ -176,6 +177,7 @@ class MainController(object):
                                 line_split[2],
                                 line_split[3]])
         sbd_entries_np = np.array(entries)
+        print(sbd_entries_np)
 
         entries = []
         for results_file in stc_result_file_list:
@@ -188,14 +190,15 @@ class MainController(object):
                 line_split = line.split(';')
 
                 # TODO: just a temporary workaround
-                if (line_split[4] == 'I'):
-                    line_split[4] = 'NA'
+                #if (line_split[4] == 'I'):
+                #    line_split[4] = 'NA'
                 entries.append([line_split[0].split('.')[0],
                                 line_split[1],
                                 line_split[2],
                                 line_split[3],
                                 line_split[4]])
         stc_entries_np = np.array(entries)
+        print(stc_entries_np)
 
         entries = []
         for results_file in cmc_result_file_list:
@@ -212,6 +215,8 @@ class MainController(object):
                                 line_split[3],
                                 line_split[4]])
         cmc_entries_np = np.array(entries)
+        print(cmc_entries_np)
+        
 
         if(len(stc_entries_np) > 0 and len(cmc_entries_np) > 0):
             entries_np = np.concatenate((sbd_entries_np, stc_entries_np[:, 4:]), axis=1)
@@ -221,7 +226,7 @@ class MainController(object):
             dummy_entries_np[:] = "NA"
             entries_np = np.concatenate((sbd_entries_np, dummy_entries_np), axis=1)
             entries_np = np.concatenate((entries_np, dummy_entries_np), axis=1)
-
+        print(entries_np)
         return entries_np
 
     def make_result_folders(self):
